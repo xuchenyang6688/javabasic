66014e9aa7a9663306ad47947bf0e58ff03e40e2
xuchenyang6688 <xuchenyang6688@163.com>
2020-07-03 12:21:23 +0800

    merge rc-0.1 to master

--- TestCompleteFuture.java
+++ TestCompleteFuture.java
@@ -175,6 +175,7 @@ public class TestCompleteFuture {
     }
 
     public static void main(String[] args) {
+        // add a comment master ----
     // Add comment by rc-0.1
     // ddddddd
 	    new TestCompleteFuture().useFuture();
